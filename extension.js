3b2c5aacc8ac9c3b6d5019b2742e95e55367be4b
Artem Seleznev <75618617+1Fiolet1@users.noreply.github.com>
2025-10-01 16:24:45 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -4,25 +4,21 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    // Регистрация команды "hello-world.sayHello"
     let disposable = vscode.commands.registerCommand('hello-world.sayHello', function () {
-        // Вывод "Hello World" в консоль и уведомление
         console.log('Hello World!');
         vscode.window.showInformationMessage('Hello World!');
     });
 
-    // Добавление команды в подписки
     context.subscriptions.push(disposable);
 
-    // Создание кнопки в нижней панели (Status Bar)
     const button = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-    button.text = '$(megaphone) Hello World'; // Текст кнопки с иконкой
-    button.tooltip = 'Click to say Hello World'; // Всплывающая подсказка
-    button.command = 'hello-world.sayHello'; // Команда, вызываемая при нажатии
-    button.show(); // Отображение кнопки
+    button.text = '$(megaphone) Hello World';
+    button.tooltip = 'Click to say Hello World';
+    button.command = 'hello-world.sayHello';
+    button.show();
     context.subscriptions.push(button);
 
-    // Вывод сообщения в консоль при активации расширения
+    
     console.log('Hello World extension is now active!');
 }
 
@@ -34,3 +30,4 @@ module.exports = {
     activate,
     deactivate
 };
+
